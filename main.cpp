688c08ce32a828913c11d507f57106be6646ab09
OliPro007 <OliPro007@gmail.com>
2016-11-13 14:19:12 -0500

    Create main.cpp stub

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+//TODO: Complete return types and parameters.
+void creerLexique(const std::string& repertoire);
+void equilibrerFlotte();
+void lancerSimulation(/*TODO: donnees de simulation*/);
+
+int main() {
+    //TODO
+    return 0;
+}
+
+void creerLexique(const std::string& repertoire) {
+    //TODO
+}
+
+void equilibrerFlotte() {
+    //TODO
+}
+
+void lancerSimulation(/*TODO: donnees de simulation*/) {
+    //TODO
+}
