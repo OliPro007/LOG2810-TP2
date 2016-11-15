9ff1d13b55f81bacc795c0edcfc35fc00b72f2f2
OliPro007 <OliPro007@gmail.com>
2016-11-15 18:05:03 -0500

    Print menu, python ftw

---
+++ automate.py
@@ -0,0 +1,11 @@
+import subprocess
+
+while(True):
+    print("====================================================")
+    print("=== Veuillez choisir une option:")
+    print("===     a: Creer les zones")
+    print("===     b: Entrer les clients et les vehicules")
+    print("===     c: Demarrer la simulation")
+    print("===     d: Quitter")
+    print("====================================================")
+    print("Votre choix: ")
