61616bcf6d103147b76dac148fb7839b6dc0018f
Jean-Francois Blain <jeanfrancois.blain@bell.ca>
2016-11-15 19:56:06 -0500

    Added fct 'main'

--- automate.py
+++ automate.py
@@ -1,11 +1,17 @@
 import subprocess
 
-while(True):
-    print("====================================================")
-    print("=== Veuillez choisir une option:")
-    print("===     a: Creer les zones")
-    print("===     b: Entrer les clients et les vehicules")
-    print("===     c: Demarrer la simulation")
-    print("===     d: Quitter")
-    print("====================================================")
-    print("Votre choix: ")
+
+def main():
+    while(True):
+        print("====================================================")
+        print("=== Veuillez choisir une option:")
+        print("===     a: Creer les zones")
+        print("===     b: Entrer les clients et les vehicules")
+        print("===     c: Demarrer la simulation")
+        print("===     d: Quitter")
+        print("====================================================")
+        print("Votre choix: ")
+
+
+if __name__ == "__main__":
+    main()
