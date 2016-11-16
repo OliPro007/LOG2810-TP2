82c6412d4f1f5703545eb94ae43855673e31be97
OliPro007 <OliPro007@gmail.com>
2016-11-15 20:27:37 -0500

    Completed menu. Now able to read input, display all files in directory and handle errors.

--- automate.py
+++ automate.py
@@ -1,16 +1,39 @@
-import subprocess
+import os
+
+
+def creer_lexique(path):
+    try:
+        for file in os.listdir(path):
+            if file.endswith(".txt"):
+                print(file)
+    except FileNotFoundError as e:
+        print("ERREUR: {err}: {dir}".format(err=e.strerror, dir=path))
 
 
 def main():
-    while(True):
+    while True:
         print("====================================================")
         print("=== Veuillez choisir une option:")
-        print("===     a: Creer les zones")
-        print("===     b: Entrer les clients et les vehicules")
-        print("===     c: Demarrer la simulation")
+        print("===     a: Créer les zones")
+        print("===     b: Entrer les clients et les véhicules")
+        print("===     c: Démarrer la simulation")
         print("===     d: Quitter")
         print("====================================================")
-        print("Votre choix: ")
+        print("Votre choix: ", end="")
+
+        choix = input()
+        if choix is 'a':
+            print("Entrez le répertoire des zones: ", end="")
+            path = input()
+            creer_lexique(path)
+        elif choix is 'b':
+            pass
+        elif choix is 'c':
+            pass
+        elif choix is 'd':
+            break
+        else:
+            print("Choix invalide.")
 
 
 if __name__ == "__main__":
