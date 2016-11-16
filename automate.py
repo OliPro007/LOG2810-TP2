028abaa63191e66b732d3f1595baeb6e216cb9ef
OliPro007 <OliPro007@gmail.com>
2016-11-15 21:27:16 -0500

    Created choice b stub

--- automate.py
+++ automate.py
@@ -3,11 +3,22 @@ import os
 
 def creer_lexique(path):
     try:
-        for file in os.listdir(path):
-            if file.endswith(".txt"):
-                print(file)
+        for file_name in os.listdir(path):
+            if file_name.endswith(".txt"):
+                print(file_name)
+                with open(path+"/"+file_name) as file:
+                    # TODO: Read the file and create the finite state automata
+                    print(file.read())
     except FileNotFoundError as e:
         print("ERREUR: {err}: {dir}".format(err=e.strerror, dir=path))
+
+
+def equilibrer_flotte():
+    pass
+
+
+def lancer_simulation():
+    pass
 
 
 def main():
@@ -27,9 +38,24 @@ def main():
             path = input()
             creer_lexique(path)
         elif choix is 'b':
-            pass
+            print("Voulez-vous entrer les informations du client à partir d'un fichier? (o/n): ", end="")
+            utiliser_fichier = input()
+
+            if utiliser_fichier is 'o':
+                file_name = input()
+                try:
+                    with open(file_name) as file:
+                        print(file.read())
+                except FileNotFoundError as e:
+                    print(e.strerror)
+
+                pass  # TODO: Complete when using a file
+            elif utiliser_fichier is 'n':
+                pass  # TODO: Complete when not using a file
+            else:
+                print("Choix invalide.")
         elif choix is 'c':
-            pass
+            pass  # TODO: Complete choice c
         elif choix is 'd':
             break
         else:
